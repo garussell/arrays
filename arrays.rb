184a0866c3badd9133ac580d1e7b08f9721eb981
Allen Russell <garyrusselljr@gmail.com>
2023-04-12 14:14:37 -0600

    Demonstrate 4 different array methods

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ animals = ["dog","cat","cow","horse"]
 integers = [56,44,3,99]
 floats = [2.34,5.5,23.1,100.1]
 booleans = [true, false, true, true]
+
+#This will remove the last element of the animals array.
+animals.pop
+
+#This will add an element to the end of the integers array.
+integers.push(15)
+
+#This will remove the first element of the floats array.
+floats.shift
+
+#This will add an element to the beginning of the booleans array.
+booleans.unshift(false)
